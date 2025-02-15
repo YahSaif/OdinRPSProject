ac78b00f06d8c5aea713acbf4a9ff3e59f2e5dc6
Saif <islamsaif0099@gmail.com>
2025-02-15 19:22:53 +0600

    final changes

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const score = {
     Losses: 0,
     Ties: 0
 };
+document.addEventListener("DOMContentLoaded", function() {
+  
+
+    function updateScore() {
+        document.querySelector('.js').innerHTML = `Wins: ${score.Wins} Losses: ${score.Losses} Ties: ${score.Ties}`;
+    }
+
+    updateScore(); 
+});
